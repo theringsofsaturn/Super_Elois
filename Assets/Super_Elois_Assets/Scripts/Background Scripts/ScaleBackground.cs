948ecad8a5bbd000b211524ab296f2fc2bb03850
Emilian Kasemi <theringsofsaturn@users.noreply.github.com>
2020-05-03 19:17:46 +0200

    changes

---
+++ Assets/Super_Elois_Assets/Scripts/Background Scripts/ScaleBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBackground : MonoBehaviour {
+
+
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+		transform.localScale = new Vector3(1, 1, 1);
+
+		float width = sr.sprite.bounds.size.x; // Kalkulojme width te fotos ne unity world space. Po ashtu dhe per height. 
+		float height = sr.sprite.bounds.size.y;
+
+		float worldHeight = Camera.main.orthographicSize * 2f;
+		float worldWidth = worldHeight / Screen.height * Screen.width;
+		Vector3 tempScale = transform.localScale;
+		tempScale.x = worldWidth / width + 0.1f;
+		tempScale.y = worldHeight / height + 0.1f;
+
+		transform.localScale = tempScale;
+	}
+	
+
+}
---
+++ Assets/Super_Elois_Assets/Scripts/Enemy Scripts/FrogScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogScript : MonoBehaviour {
+
+	private Animator anim;
+
+	private bool animation_Started;
+	private bool animation_Finished;
+
+	private int jumpedTimes;
+	private bool jumpLeft = true;
+
+	private string coroutine_Name = "FrogJump";
+
+	void Awake() {
+		anim = GetComponent<Animator>();
+	}
+
+
+	void Start () {
+		StartCoroutine(coroutine_Name);
+	}
+	
+	
+	void Update () {
+		
+	}
+
+	IEnumerator FrogJump() {
+		yield return new WaitForSeconds(Random.Range(1f, 4f));
+
+		if(jumpLeft) {
+			anim.Play("FrogJumpLeft");
+		} else {
+		
+		}
+	}
+}
